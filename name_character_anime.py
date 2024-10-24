a155d338350a5f8f27b6ff6ef19c4b32daf16649
LilLily1999 <138665798+LilLily1999@users.noreply.github.com>
2024-10-24 16:06:38 -0400

    Update name_character_anime.py

--- name_character_anime.py
+++ name_character_anime.py
@@ -1,17 +1,24 @@
+# Import module
 import urllib.request
 import json
 
+# URL for API
 url = 'https://animechan.io/api/v1/quotes/random'
 
 print('Retrieving', url)
 
+# Use the server
 req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
 
+
 try:
+    # Open the URL and retrieve data
     uh = urllib.request.urlopen(req)
     data = uh.read().decode()
     print('Retrieved', len(data), 'characters')
     data_dict = json.loads(data)
+    
+    # Retrieve character name
     character_name = data_dict['data']['character']['name']
     print(f"This is a character name in a anime: {character_name}")
     
